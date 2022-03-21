4d991344f22092a25c4cd9efa18795fed7c428ba
marzan131390 <marzandwmtec@gmail.com>
2022-03-22 01:29:36 +0600

    deposit part completer

--- js/bankScript.js
+++ js/bankScript.js
@@ -1,8 +1,31 @@
 //Define user interface...
 const logButton = document.getElementById("loginBtn");
 let loginArea = document.querySelector(".login-area");
+let transactionArea = document.getElementById("transaction-area");
+const depositButton = document.getElementById("deposit-btn");
+const withdrawButton = document.querySelector("#withdraw-btn");
+let depositCar = document.querySelector("#current-deposit");
+let withdrawCar = document.querySelector("#current-withdraw");
+let balanceCar = document.querySelector("#current-balance");
+const depositInput = document.getElementById("deposit-amount");
+const withdrawInput = document.getElementById("withdraw-amount");
 
-//Adding event listener...
+//Login Button Event Handler
 logButton.addEventListener("click", function(){
-    loginArea.style.display = "none"
+    loginArea.style.display = "none";
+    transactionArea.style.display = "block";
+});
+
+//Deposit Button Event Handler....
+depositButton.addEventListener("click", function() {
+    let depositMoney = parseFloat(depositInput.value);
+    let currentDeposit = parseFloat(depositCar.innerText);
+    let totalDeposit = currentDeposit + depositMoney;
+    depositCar.innerText = totalDeposit;
+    depositInput.value = "";
+    //Subtraction from Current Balance Amount
+    let currentBalance = parseFloat(balanceCar.innerText);
+    let totalBalance = currentBalance - depositMoney;
+    balanceCar.innerText = totalBalance;
+
 })
