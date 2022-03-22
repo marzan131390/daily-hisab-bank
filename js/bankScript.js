f3327c866f2176be970be9c752d6fb6dcdbeb54c
marzan131390 <marzandwmtec@gmail.com>
2022-03-22 22:11:38 +0600

    deposit balance part fully complete

--- js/bankScript.js
+++ js/bankScript.js
@@ -4,9 +4,9 @@ let loginArea = document.querySelector(".login-area");
 let transactionArea = document.getElementById("transaction-area");
 const depositButton = document.getElementById("deposit-btn");
 const withdrawButton = document.querySelector("#withdraw-btn");
-let depositCar = document.querySelector("#current-deposit");
-let withdrawCar = document.querySelector("#current-withdraw");
-let balanceCar = document.querySelector("#current-balance");
+let currentDeposit = document.querySelector("#current-deposit");
+let currentWithdraw = document.querySelector("#current-withdraw");
+let currentBalance = document.querySelector("#current-balance");
 const depositInput = document.getElementById("deposit-amount");
 const withdrawInput = document.getElementById("withdraw-amount");
 
@@ -18,14 +18,43 @@ logButton.addEventListener("click", function(){
 
 //Deposit Button Event Handler....
 depositButton.addEventListener("click", function() {
-    let depositMoney = parseFloat(depositInput.value);
-    let currentDeposit = parseFloat(depositCar.innerText);
-    let totalDeposit = currentDeposit + depositMoney;
-    depositCar.innerText = totalDeposit;
-    depositInput.value = "";
-    //Subtraction from Current Balance Amount
-    let currentBalance = parseFloat(balanceCar.innerText);
-    let totalBalance = currentBalance - depositMoney;
-    balanceCar.innerText = totalBalance;
+    // let depositMoney = parseFloat(depositInput.value);
+    
+    // getInputNumber(depositInput)
+    // let currentDeposit = parseFloat(depositCar.innerText);
+    // let totalDeposit = currentDeposit + depositMoney;
+    // depositCar.innerText = totalDeposit;
 
+    let depositMoney = getInputNumber(depositInput);
+    //Updating Deposit Value...
+    updateSpanText(currentDeposit, depositMoney);
+   
+    //Subtraction from Current Balance Amount
+    updateSpanText(currentBalance, depositMoney);
+    depositInput.value = "";
+});
+
+//Creating A Function to Get Input Number..
+function getInputNumber(id) {
+    let inputValue = parseFloat(id.value);
+    return inputValue;
+    
+}
+
+//Withdraw Button Event Handler...
+withdrawButton.addEventListener("click", function() {
+    // const withdrawAmount = parseFloat(withdrawInput.value);
+    
+    let withdrawAmount = getInputNumber(withdrawInput);
+    // const currentWithMoney = parseFloat(currentWithdraw.innerText);
+    // let totalAmount = currentWithMoney + withdrawAmount;
+    // currentWithdraw.innerText = totalAmount;
+    updateSpanText(currentWithdraw, withdrawAmount);
+    withdrawInput.value = "";
 })
+
+function updateSpanText(id, insertMoney) {
+    let currentValue = parseFloat(id.innerText);
+    let totalAmount = currentValue + insertMoney;
+    id.innerText = totalAmount;
+}
